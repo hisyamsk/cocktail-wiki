1be1d717ee62ef6dce3ebf2ee477b3e895f8c9ec
hisyamsk <hisyamskurniawan18@gmail.com>
2021-10-22 23:34:02 +0700

    adjust singlecocktail functionality

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams, Link } from "react-router-dom";
 import Loading from "../components/Loading";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
@@ -6,28 +6,92 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 const SingleCocktail = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
+  const [cocktail, setCocktail] = useState(null);
 
-  const fetchCocktail = async () => {
+  const fetchCocktail = useCallback(async () => {
     try {
       const response = await fetch(`${url}${id}`);
       const data = await response.json();
-      const { drinks } = data;
-      const { idDrink } = drinks[0];
-      console.log(idDrink);
+      if (data.drinks) {
+        const { drinks } = data;
+        const drink = drinks[0];
+        let ingredients = [];
+
+        for (let i = 1; i <= 15; i++) {
+          if (drink[`strIngredient${i}`]) {
+            ingredients.push(drink[`strIngredient${i}`]);
+          }
+        }
+
+        const {
+          strDrink: name,
+          strDrinkThumb: image,
+          strCategory: category,
+          strAlcoholic: type,
+          strGlass: glass,
+          strInstructions: instructions,
+        } = drink;
+
+        const newCocktail = {
+          name,
+          image,
+          category,
+          type,
+          glass,
+          instructions,
+          ingredients: ingredients,
+        };
+        setCocktail(newCocktail);
+      }
     } catch (error) {
       console.log(error);
     }
     setLoading(false);
-  };
+  }, [id]);
 
   useEffect(() => {
     setLoading(true);
     fetchCocktail();
-  }, []);
+  }, [id, fetchCocktail]);
 
   return (
     <div className="main-section container">
-      {loading ? <Loading /> : <h2>SingleCocktail Component</h2>}
+      {loading ? (
+        <Loading />
+      ) : !cocktail ? (
+        <h2 className="title">no cocktail to display</h2>
+      ) : (
+        <div className="drink">
+          <img src={cocktail.image} alt={cocktail.name} />
+          <div className="detail-wrapper">
+            <h1>
+              {cocktail.name} <span>({cocktail.type})</span>
+            </h1>
+            <div className="drink-details">
+              <p>
+                <span>Category: </span>
+                {cocktail.category}
+              </p>
+              <p>
+                <span>Glass Type: </span>
+                {cocktail.glass}
+              </p>
+              <p>
+                <span>Ingredients:</span>
+              </p>
+              <ul>
+                {cocktail.ingredients.map((item, index) => {
+                  return <li key={index}>{item}</li>;
+                })}
+              </ul>
+              <p>
+                <span>Instructions: </span>
+                {cocktail.instructions}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
